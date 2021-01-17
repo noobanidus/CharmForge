a69f06d4d4d93b68152dea5f325e3f875ba5a7dd
f43nd1r <lukas.morawietz@gmail.com>
2021-01-17 21:46:05 +0100

    less warnings. Closes #42

--- src/main/java/svenhjol/charm/mixin/MineshaftPiecesMixin.java
+++ src/main/java/svenhjol/charm/mixin/MineshaftPiecesMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.world.ISeedReader;
 import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.feature.structure.MineshaftPieces;
 import net.minecraft.world.gen.feature.structure.StructureManager;
 import net.minecraft.world.gen.feature.structure.StructurePiece;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,18 +16,18 @@ import svenhjol.charm.module.MineshaftImprovements;
 
 import java.util.Random;
 
-@Mixin(targets = {
-    "net.minecraft.world.gen.feature.structure.MineshaftPieces$Corridor",
-    "net.minecraft.world.gen.feature.structure.MineshaftPieces$Room"
+@Mixin(value = {
+        MineshaftPieces.Corridor.class,
+        MineshaftPieces.Room.class
 })
 public class MineshaftPiecesMixin {
     @Inject(
-        method = "func_230383_a_(Lnet/minecraft/world/ISeedReader;Lnet/minecraft/world/gen/feature/structure/StructureManager;Lnet/minecraft/world/gen/ChunkGenerator;Ljava/util/Random;Lnet/minecraft/util/math/MutableBoundingBox;Lnet/minecraft/util/math/ChunkPos;Lnet/minecraft/util/math/BlockPos;)Z",
-        at = @At("RETURN")
+            method = "func_230383_a_(Lnet/minecraft/world/ISeedReader;Lnet/minecraft/world/gen/feature/structure/StructureManager;Lnet/minecraft/world/gen/ChunkGenerator;Ljava/util/Random;Lnet/minecraft/util/math/MutableBoundingBox;Lnet/minecraft/util/math/ChunkPos;Lnet/minecraft/util/math/BlockPos;)Z",
+            at = @At("RETURN")
     )
     private void hookGenerate(ISeedReader world, StructureManager structure, ChunkGenerator gen, Random rand, MutableBoundingBox box, ChunkPos chunkPos, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
         if (cir.getReturnValue()) // if a piece did generate
-            MineshaftImprovements.generatePiece((StructurePiece)(Object)this, world, structure, gen, rand, box, chunkPos, blockPos);
+            MineshaftImprovements.generatePiece((StructurePiece) (Object) this, world, structure, gen, rand, box, chunkPos, blockPos);
     }
 }
 
